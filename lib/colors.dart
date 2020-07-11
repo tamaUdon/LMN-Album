5365b344598f685aebd6fc6189abb1ecee18433b
tamaUdon <sakurayuki603@gmail.com>
2020-07-11 23:01:53 +0900

    add new button color

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,5 +4,6 @@ const kLightGreen = const Color(0xFFC1E1DC);
 const kMossGreen = const Color(0xFF4D5A58);
 const kBrown900 = const Color(0xFF393737);
 const kButtonColor = const Color(0xFFECF5F4);
+const kLightYellow = const Color(0xFFFFEB94);
 const kWhite = Colors.white;
 const kErrorRed = Colors.red;
