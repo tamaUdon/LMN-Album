9dad74bdb0739a4b6124cd29387ce897bd066642
tamaUdon <sakurayuki603@gmail.com>
2020-07-11 23:02:14 +0900

    add onTapped method

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:albumapp/home.dart';
 import 'package:flutter/material.dart';
 
 import 'colors.dart';
@@ -8,6 +9,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {  
+
+  String inputText = "";
+  String placeholder = "Input your passcord";
+  String showText = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,31 +21,69 @@ class _LoginPageState extends State<LoginPage> {
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 30.0),
           children: <Widget>[
-            SizedBox(height: 80),
+            
+            SizedBox(height: 50),
             Column(
               children: <Widget>[
                 SizedBox(height: 16.0),
+                // MARK: - アイコン
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Icon(
+                  Icons.lock
+                  ),
+                ),
+                // MARK: - タイトル
                 Text(
-                  'LMN',
+                  'LeMoN',
                   style: Theme.of(context).textTheme.headline5,
+                ),
+                // テキスト
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: showText.isEmpty ? 
+                    Text(
+                      placeholder,
+                      style: TextStyle(
+                        fontSize: 20.0
+                      ),
+                    ) 
+                    : Text(
+                    showText,
+                    style: TextStyle(
+                      fontSize: 20.0
+                    ),
+                  )
                 ),
               ],
             ),
+            Divider(
+              color: Colors.black,
+              height: 10,
+              thickness: 1,
+              indent: 20,
+              endIndent: 20,
+            ),
             // MARK: - 番号ボタン
             Padding(
-              padding: EdgeInsets.only(top: 50.0),
+              padding: EdgeInsets.only(top: 20.0),
               child: Column (
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 for (var i = 0; i < 3; i++)
                 Padding(
-                  padding: EdgeInsets.all(20.0),
+                  padding: EdgeInsets.all(25.0),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
                       for (var j = 0; j < 3; j++)
                       RawMaterialButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            inputText = (1+j+(3*i)).toString();
+                            showText += "*";
+                          });
+                        },
                         elevation: 2.0,
                         fillColor: Colors.white,
                         child: Text(
@@ -55,6 +99,22 @@ class _LoginPageState extends State<LoginPage> {
                     ],
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: RawMaterialButton(
+                    onPressed: () {},
+                    elevation: 2.0,
+                    fillColor: Colors.white,
+                    child: Text("0",
+                      style: TextStyle(
+                        fontSize: 20,
+                        //fontFamily: ,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(20.0),
+                    shape: CircleBorder(),
+                  ),
+                )
               ],
             ),
           ),
@@ -62,26 +122,35 @@ class _LoginPageState extends State<LoginPage> {
             children: <Widget>[
               FlatButton(
                 child: Text('CANCEL'),
-                shape: BeveledRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(7.0))
-                  ),
-                  onPressed: (){
-                    // _userNameController.clear();
-                    // _userNameController.clear();
-                  },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
+                onPressed: () {
+                  setState(() {
+                    inputText = "";
+                    showText = "";
+                  });
+                },
+              ),
                 RaisedButton(
                   child: Text('NEXT'),
                   elevation: 8.0,
-                  shape: BeveledRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(7.0))
-                    ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                   onPressed: (){
-                    Navigator.pop(context);
+                    // TODO: ここにパスコードチェックロジック
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return HomePage();
+                        },
+                      ),
+                    );
                   },
                 ),
               ],
-            )
+            ),
           ],
         )
       )
@@ -89,6 +158,26 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
+class NumButtonController extends TextEditingController{
+  String inputText = "";
+  String showText = "";
+
+  numButtonPressed(int number){
+    showText += "*";
+    inputText += number.toString();
+  }
+
+  @override
+  clear(){
+    inputText = "";
+    showText = "";
+  }
+
+  register(){
+    // TODO: not yet implement
+    print("login text" + text);
+  }
+}
 
 class AccentColorOverride extends StatelessWidget {
   const AccentColorOverride({Key key, this.color, this.child})
