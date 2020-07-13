7eb3399371e578cf14082906c56d9ea0075c8ded
tamaUdon <sakurayuki603@gmail.com>
2020-07-13 23:07:10 +0900

    add ListView

--- lib/home.dart
+++ lib/home.dart
@@ -1,36 +1,72 @@
 import 'package:albumapp/colors.dart';
 import 'package:flutter/material.dart';
 
-class Content{
-  // image
-  // text
-}
+import 'model/content.dart';
+import 'model/dummy_content_repository.dart';
 
 class HomePage extends StatelessWidget {
 
-  //final List<Content> _titles = Content.titles;
+  final diaries = ContentRepository.loadContents();
+  HomePage();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.only(top: 40.0),
-        color: kLightGreen,
-        child: ListView(
-          children: _titles
-            .map((Content c) => _buildContent(c, context))
-            .toList()),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: kButtonColor,
+        appBar: AppBar(
+          centerTitle: true, 
+          title: new Text(
+            "LeMoN", 
+            style: TextStyle(color: kBrown900)
           ),
-        );
-      } 
-    }
-
-Widget _buildContent(Content category, BuildContext context) {
-    final categoryString =
-        category.toString().replaceAll('Category.', '').toUpperCase();
-    final ThemeData theme = Theme.of(context);
-    return GestureDetector(
-      //onTap: () => onCategoryTap(category),
-      child: // ListView
+          backgroundColor: kLightGreen,
+        ),
+        body: ListView.separated(
+          separatorBuilder: (BuildContext context, int index) {
+            return separatorItem();
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return _messageItem(diaries[index]);
+          },
+          itemCount: diaries.length,
+        )
+      )
     );
   }
+}
+Widget separatorItem() {
+  return Container(
+    height: 10,
+    color: kButtonColor,
+  );
+}
+
+Widget _messageItem(Diary diary) {
+  return Container(
+    decoration: new BoxDecoration(
+      border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey)),
+      color: kWhite
+    ),
+    child: ListTile(
+      leading: Icon(Icons.cake),
+      title: Text(
+        diary.title,
+        style: TextStyle(
+          color:kBrown900,
+          fontSize: 18.0
+        ),
+      ),
+      subtitle: Text(diary.memo),
+      trailing: Icon(Icons.more_vert),
+      contentPadding: EdgeInsets.all(10.0),
+
+      onTap: () {
+        print("onTap called.");
+      }, // タップ
+      onLongPress: () {
+        print("onLongTap called.");
+      }, // 長押し
+    ),
+  );
+}
