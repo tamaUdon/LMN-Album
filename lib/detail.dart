1143bd7556db14752c78de37368bb1e7797fe6f3
tamaUdon <sakurayuki603@gmail.com>
2020-07-21 23:06:20 +0900

    add diary's title

--- lib/detail.dart
+++ lib/detail.dart
@@ -5,7 +5,29 @@ import 'package:flutter/material.dart';
 class ModalDiaryDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(30.0),
+          children: <Widget>[
+            SizedBox(height: 50.0),
+            Column(
+              children: <Widget>[
+                SizedBox(height: 16.0),
+                // MARK: - タイトル入力欄
+                new TextField(
+                  enabled: true,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    labelText: 'Title'
+                  ),
+                )
+                
+              ],
+            ),
+          ],
+        )
+      ,)
+    );
   }
 }
