73feffe8650b58430d132d8b684951bb3fbb9ecd
tamaUdon <sakurayuki603@gmail.com>
2020-07-24 00:24:10 +0900

    add new color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 const kLightGreen = const Color(0xFFC1E1DC);
+const kShadowGreen = const Color(0xFF637C78);
 const kMossGreen = const Color(0xFF4D5A58);
+const kAccentGreen = const Color(0xFF40E0D0);
 const kBrown900 = const Color(0xFF393737);
+const kLightGray = const Color(0xFFC1C1C1);
 const kButtonColor = const Color(0xFFECF5F4);
 const kLightYellow = const Color(0xFFFFEB94);
 const kWhite = Colors.white;
 const kErrorRed = Colors.red;
+const kAccentBlue = const Color(0xFF0091C5);
