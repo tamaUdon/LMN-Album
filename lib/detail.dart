56b6ba0f416d0ece4ea12af6524671852483e41a
tamaUdon <sakurayuki603@gmail.com>
2020-07-24 00:24:42 +0900

    add photo picker to details page

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,16 +1,44 @@
 
 
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ModalDiaryDetail extends StatelessWidget{
+import 'package:albumapp/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ModalDiaryDetail extends StatefulWidget{
+  @override
+  _ModalDiarydetailState createState() => _ModalDiarydetailState();
+}
+
+class _ModalDiarydetailState extends State<ModalDiaryDetail>{
+  File _image;
+  final picker = ImagePicker();
+  
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+  }
+
+  void _onOKTapped(){
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return 
+    Scaffold(
+      appBar: AppBar(
+        backgroundColor: kLightGreen,
+        title: Text('New Diary'),
+      ),
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.all(30.0),
           children: <Widget>[
-            SizedBox(height: 50.0),
             Column(
               children: <Widget>[
                 SizedBox(height: 16.0),
@@ -21,13 +49,74 @@ class ModalDiaryDetail extends StatelessWidget{
                   decoration: const InputDecoration(
                     labelText: 'Title'
                   ),
+                ),
+                // MARK: - テキストフィールド
+                Card(
+                  color: kLightGreen,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      maxLength: 100,
+                      maxLines: 10,
+                      decoration: InputDecoration.collapsed(hintText: "Enter your text here")
+                    )
+                  ),
+                ),
+                // MARK: - 画像ピッカー
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: 
+                  Card(
+                    borderOnForeground: true,
+                    child: Center(
+                      child:
+                      _image == null
+                      // 画像ピッカーボタン
+                      ? ButtonBar(
+                        alignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            FlatButton(
+                              child: 
+                                Text(
+                                  'Tap to select image',
+                                  style: TextStyle(
+                                    color: kBrown900,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              onPressed: getImage,
+                            )
+                          ],
+                        )
+                      // 画像選択後、画像表示する
+                      : Image.file(_image) // TODO: タップ可能にして再度選択可能にする
+                    ),
+                  )
                 )
-                
-              ],
+              ]
             ),
+            // OKボタン
+            Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: 
+                RaisedButton(
+                  color: kLightYellow,
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Text(
+                    'OK',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  onPressed: _onOKTapped,
+                ),
+            )
           ],
-        )
-      ,)
+        ),
+      ),
     );
   }
 }
