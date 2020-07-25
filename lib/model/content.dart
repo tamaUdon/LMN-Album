155552e24a61b2b896736089206eea7ab1dd39ec
tamaUdon <sakurayuki603@gmail.com>
2020-07-25 16:53:40 +0900

    add date property

--- lib/model/content.dart
+++ lib/model/content.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // Diaryの種類でソートする機能が必要な場合
-enum Sort { day, month, year } 
+enum Sort { list, tile } 
 
 class Diary {
   const Diary({
@@ -10,16 +10,19 @@ class Diary {
     @required this.id,
     @required this.title,
     @required this.memo,
+    @required this.date,
     this.image,
   })  : assert(id != null),
         assert(title != null),
-        assert(memo != null);
+        assert(memo != null),
+        assert(date != null);
 
   //final Content content;
   final int id;
   final String title;
   final String memo;
   final Image image;
+  final DateTime date;
 
   //String get assetName => '$id-0.jpg';
   //String get assetPackage => 'shrine_images';
