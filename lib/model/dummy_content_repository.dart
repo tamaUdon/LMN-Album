26d033bc2a96be98e947db5eb337afbf6a67e7ae
tamaUdon <sakurayuki603@gmail.com>
2020-07-25 16:54:01 +0900

    add date property and image property

--- lib/model/dummy_content_repository.dart
+++ lib/model/dummy_content_repository.dart
@@ -1,37 +1,51 @@
+import 'package:flutter/material.dart';
+
 import 'content.dart';
 
 class ContentRepository{
   static List<Diary> loadContents(){
-    const allContents = <Diary>[
+    final allContents = <Diary>[
       Diary(
         id: 0,
         title: "Cafe",
-        memo: "I had a fun time!"
+        memo: "I had a fun time!",
+        date: DateTime.now(),
+        image: Image.asset('images/cafe.jpg'),
       ),
       Diary(
         id: 0,
         title: "Sea",
-        memo: "I had a fun time!"
+        memo: "I had a fun time!",
+        date: DateTime.now(),
+        image: Image.asset('images/sea.jpg'),
       ),
       Diary(
         id: 1,
         title: "Museum",
-        memo: "I had a fun time!"
+        memo: "I had a fun time!",
+        date: DateTime.now(),
+        image: Image.asset('images/museum.jpg'),
       ),
       Diary(
         id: 2,
         title: "FootBall",
-        memo: "I had a fun time!"
+        memo: "I had a fun time!",
+        date: DateTime.now(),
+        image: Image.asset('images/studium.jpg'),
       ),
       Diary(
         id: 3,
         title: "Bar",
-        memo: "I had a fun time!"
+        memo: "I had a fun time!",
+        date: DateTime.now(),
+        image: Image.asset('images/bar.jpg'),
       ),
       Diary(
         id: 4,
         title: "Reading Book",
-        memo: "I had a fun time!"
+        memo: "I had a fun time!",
+        date: DateTime.now(),
+        image: Image.asset('images/reading_book.jpeg'),
       ),
     ];
     return allContents;
