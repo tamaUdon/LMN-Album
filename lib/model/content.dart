fc21544d617c6240b8407567e78d191286945fb2
tamaUdon <sakurayuki603@gmail.com>
2020-07-26 23:48:53 +0900

    add objectbox entity

--- lib/model/content.dart
+++ lib/model/content.dart
@@ -1,32 +1,57 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import "package:objectbox/objectbox.dart";
+//import 'objectbox.g.dart';
 
 // Diaryの種類でソートする機能が必要な場合
 enum Sort { list, tile } 
 
+// ダミーデータ
+// class Diary {
+//   const Diary({
+//     //@required this.content,
+//     @required this.id,
+//     @required this.title,
+//     @required this.memo,
+//     @required this.date,
+//     this.image,
+//   })  : assert(id != null),
+//         assert(title != null),
+//         assert(memo != null),
+//         assert(date != null);
+
+//   //final Content content;
+//   final int id;
+//   final String title;
+//   final String memo;
+//   final Image image;
+//   final DateTime date;
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': this.id,
+//       'title': this.title,
+//       'memo': this.memo,
+//       'date': this.date,
+//       'image': this.image,
+//     };
+//   }
+// }
+
+@Entity()
 class Diary {
-  const Diary({
-    //@required this.content,
-    @required this.id,
-    @required this.title,
-    @required this.memo,
-    @required this.date,
-    this.image,
-  })  : assert(id != null),
-        assert(title != null),
-        assert(memo != null),
-        assert(date != null);
+    @Id()
+    int id;
+    String title;
+    String memo;
+    DateTime date;
+    Image image;
+    
+    Diary(); 
 
-  //final Content content;
-  final int id;
-  final String title;
-  final String memo;
-  final Image image;
-  final DateTime date;
+    Diary.construct(this.title, this.memo, this.image){
+      date = DateTime.now();
+      print("constructed date: $date");
+    }
 
-  //String get assetName => '$id-0.jpg';
-  //String get assetPackage => 'shrine_images';
-
-  //@override
-  //String toString() => "$name (id=$id)";
+    toString() => "Diary{id: $id, title: $title, memo: $memo, date: $date, image: $image}";
 }
