f2e6e2cfcd826ec3407c98eeebaba036bc42f92a
tamaUdon <sakurayuki603@gmail.com>
2020-07-28 23:40:03 +0900

    remove dummy data

--- lib/model/dummy_content_repository.dart
+++ lib/model/dummy_content_repository.dart
@@ -1,47 +1,47 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'content.dart';
+// import 'content.dart';
 
-class ContentRepository{
-  static List<Diary> loadContents(){
-    final allContents = <Diary>[
-      Diary(
-        title: "Cafe",
-        memo: "I had a fun time!",
-        date: DateTime.now(),
-        image: Image.asset('images/cafe.jpg'),
-      ),
-      Diary(
-        title: "Sea",
-        memo: "I had a fun time!",
-        date: DateTime.now(),
-        image: Image.asset('images/sea.jpg'),
-      ),
-      Diary(
-        title: "Museum",
-        memo: "I had a fun time!",
-        date: DateTime.now(),
-        image: Image.asset('images/museum.jpg'),
-      ),
-      Diary(
-        title: "FootBall",
-        memo: "I had a fun time!",
-        date: DateTime.now(),
-        image: Image.asset('images/studium.jpg'),
-      ),
-      Diary(
-        title: "Bar",
-        memo: "I had a fun time!",
-        date: DateTime.now(),
-        image: Image.asset('images/bar.jpg'),
-      ),
-      Diary(
-        title: "Reading Book",
-        memo: "I had a fun time!",
-        date: DateTime.now(),
-        image: Image.asset('images/reading_book.jpeg'),
-      ),
-    ];
-    return allContents;
-  }
-}
+// class ContentRepository{
+//   static List<Diary> loadContents(){
+//     final allContents = <Diary>[
+//       Diary(
+//         title: "Cafe",
+//         memo: "I had a fun time!",
+//         date: DateTime.now(),
+//         image: Image.asset('images/cafe.jpg'),
+//       ),
+//       Diary(
+//         title: "Sea",
+//         memo: "I had a fun time!",
+//         date: DateTime.now(),
+//         image: Image.asset('images/sea.jpg'),
+//       ),
+//       Diary(
+//         title: "Museum",
+//         memo: "I had a fun time!",
+//         date: DateTime.now(),
+//         image: Image.asset('images/museum.jpg'),
+//       ),
+//       Diary(
+//         title: "FootBall",
+//         memo: "I had a fun time!",
+//         date: DateTime.now(),
+//         image: Image.asset('images/studium.jpg'),
+//       ),
+//       Diary(
+//         title: "Bar",
+//         memo: "I had a fun time!",
+//         date: DateTime.now(),
+//         image: Image.asset('images/bar.jpg'),
+//       ),
+//       Diary(
+//         title: "Reading Book",
+//         memo: "I had a fun time!",
+//         date: DateTime.now(),
+//         image: Image.asset('images/reading_book.jpeg'),
+//       ),
+//     ];
+//     return allContents;
+//   }
+// }
